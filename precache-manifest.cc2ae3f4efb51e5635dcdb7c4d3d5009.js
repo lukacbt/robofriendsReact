680517e268454f5328fb6584af9bb910b59a8c1c
Luka Kukina <lukscbt@gmail.com>
2020-04-20 17:13:32 +0200

    Updates

---
+++ precache-manifest.cc2ae3f4efb51e5635dcdb7c4d3d5009.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8d3597ea77e3154cd2d3fcfbc7fa8c",
+    "url": "/robofriendsReact/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/robofriendsReact/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2ab589f2f8b292cd067d",
+    "url": "/robofriendsReact/static/css/main.172d6c4f.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/robofriendsReact/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendsReact/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab589f2f8b292cd067d",
+    "url": "/robofriendsReact/static/js/main.3f73f21a.chunk.js"
+  },
+  {
+    "revision": "3d8b31c509cad77cad00",
+    "url": "/robofriendsReact/static/js/runtime-main.5c3f8f52.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsReact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.6e8735ca4ab76ca37394ad6befc2b799.js"
+  "/robofriendsReact/precache-manifest.cc2ae3f4efb51e5635dcdb7c4d3d5009.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriendsReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
